ec9b2534b030c348c0cc1dd2ce747b868ed45e25
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-09-09 19:20:18 +0300

    **

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="home" className="w-full border-2 border-red-500 p-2"></section>
+  );
+};
+
+export default Hero;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  CustomerReviews,
+  Subscribe,
+  SpecialOffer,
+  Footer,
+};
